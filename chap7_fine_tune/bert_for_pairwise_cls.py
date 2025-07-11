69ff7c9ed1b029a4c52df7bc704589500587a5b5
Shi XiuFeng <shixiufengvip@outlook.com>
2025-07-11 22:40:49 +0800

    更新变量名`self.bert_encoder`为`self.bert`并相应更新`forward`方法中的变量名

--- chap7_fine_tune/bert_for_pairwise_cls.py
+++ chap7_fine_tune/bert_for_pairwise_cls.py
@@ -9,13 +9,13 @@ class BertForPairwiseCLS(BertPreTrainedModel):
 
     def __init__(self, config: BertConfig) -> None:
         super().__init__(config)
-        self.bert_encoder: BertModel = BertModel(config, add_pooling_layer=False)
+        self.bert: BertModel = BertModel(config, add_pooling_layer=False)
         self.dropout: Dropout = Dropout(config.hidden_dropout_prob)
         self.classifier: Linear = Linear(768, 2)
         self.post_init()
 
     def forward(self, x: BatchEncoding) -> Tensor:
-        bert_outputs: BaseModelOutputWithPoolingAndCrossAttentions = self.bert_encoder(**x)
+        bert_outputs: BaseModelOutputWithPoolingAndCrossAttentions = self.bert(**x)
         cls_vectors: Tensor = bert_outputs.last_hidden_state[:, 0, :]
         cls_vectors: Tensor = self.dropout(cls_vectors)
         logits: Tensor = self.classifier(cls_vectors)
